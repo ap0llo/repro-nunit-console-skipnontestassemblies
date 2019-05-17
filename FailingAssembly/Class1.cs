0c31a78772dd49b8c3bb44b653b4601d70c4380a
Andreas Gruenwald <andreasgruenwald@live.de>
2019-05-17 21:57:57 +0200

    Initial commit

---
+++ FailingAssembly/Class1.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace FailingAssembly
+{
+    public class Class1
+    {        
+        public void Method1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ WorkingAssembly/Class1.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace WorkingAssembly
+{
+    public class Class1
+    {        
+        public void Method1()
+        {            
+        }
+
+    }
+}
